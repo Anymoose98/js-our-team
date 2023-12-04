fc2f0770361807be2a96b9646dd060d6c3caaafb
Anymoose98 <daniel.digiacomo98@gmail.com>
2023-12-04 19:03:34 +0100

    Milestone 0 fatto

---
+++ js/script.js
@@ -0,0 +1,39 @@
+let dipendenti = [
+    {
+        "nome" : "Wayne Barnett",
+        "role" : "Founder & CEO",
+        "image" : "wayne-barmett-founder-ceo.jpg",
+    },
+
+    {
+        "nome" : "Angela Caroll",
+        "role" : "Chief Editor",
+        "image" : "angela-caroll-chief-editor.jpg",
+    },
+
+    {
+        "nome" : "Walter Gordon",
+        "role" : "Office Manager",
+        "image" : "walter-gordon-office-menager.jpg",
+    },
+
+    {
+        "nome" : "Angela Lopez",
+        "role" : "Social Media Manager",
+        "image" : "angela-lopez-social-media-manager.jpg",
+    },
+
+    {
+        "nome" : "Scott Estrada",
+        "role" : "Developer",
+        "image" : "scott-estrada-developer.jpg",
+    },
+
+    {
+        "nome" : "Barbara Ramos",
+        "role" : "Graphic Designer",
+        "image" : "barbara-ramos-graphic-designer.jpg",
+    },
+]
+
+console.log(dipendenti)
